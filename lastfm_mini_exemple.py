6113762d9535c1228739e775d912324caab6c5f0
duracell013 <29918045+duracell013@users.noreply.github.com>
2022-03-19 10:24:04 +0000

    Added spotipy timeout and retries

--- lastfm_mini_exemple.py
+++ lastfm_mini_exemple.py
@@ -22,7 +22,8 @@ REFRESH = True
 def connect_spotipy():
     '''Connect to Spotify API'''
     print('Connecting to spotipy...', end=' ')
-    api = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+    api = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(),
+                          requests_timeout=10, retries=10)
     print('[OK]')
     return api
 
