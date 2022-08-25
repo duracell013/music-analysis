7ba4cd5531fdc4a3e6ab2e2c221a056285809cca
duracell013 <29918045+duracell013@users.noreply.github.com>
2022-08-25 11:14:06 +0100

    Update spotify.py

--- spotify.py
+++ spotify.py
@@ -2,9 +2,11 @@
 
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import pandas as pd
+
 
 class Spotify:
-    
+
     def __init__(self):
         '''Connect to Spotify API'''
         print('Connecting to spotipy...', end=' ')
@@ -26,7 +28,8 @@ class Spotify:
             query = f'track:{title}'
         results = self.api.search(q=query, type='track')
         if results['tracks']['total'] == 0:
-            results = self.api.search(q=f'track:{title} artist:{artist}', type='track')
+            results = self.api.search(q=f'track:{title} artist:{artist}',
+                                      type='track')
         if results['tracks']['total'] == 0:
             return None, None
         else:
@@ -53,3 +56,44 @@ class Spotify:
             results = self.api.artist(uri)
             genres.extend(results['genres'])
         return genres
+
+    def playlist_tracks(self, playlist_id):
+        df = pd.DataFrame(columns=['artist', 'album', 'track'])
+        limit = 100
+        offset = 0
+        tracks = self.api.playlist_tracks(playlist_id, offset=offset, limit=1)
+        total = tracks['total']
+        while offset < total:
+            tracks = self.api.playlist_tracks(playlist_id,
+                                              offset=offset, limit=limit)
+            for i, t in enumerate(tracks['items']):
+                df.loc[i+offset] = track_info(t)
+            offset += limit
+        return df
+
+
+def sanitize(string):
+    '''Remove special characters from string to make HTTP query'''
+    string = unidecode(string)
+    chars = ['.', '&', '  ']
+    for i in chars:
+        string = string.replace(i, ' ')
+    return string.strip()
+
+
+def track_info(track):
+    '''Format and return dictionnary with artists, track, and album'''
+    artist = ', '.join([a['name'] for a in track['track']['artists']])
+    dic = {'artist': artist,
+           'track': track['track']['name'],
+           'album': track['track']['album']['name']}
+    return dic
+
+
+if __name__ == '__main__':
+    
+    playlist_id = '0k5hJzzAvLllBUJYEJddJT'  # Gilles Peterson Worldwide Radio
+
+    sp = Spotify()
+    tracks = sp.playlist_tracks(playlist_id)
+    print(tracks)
